8d950616195c3ab4daa5a2d4cf25a59090a31447
MaxPhilb <79253830+MaxPhilb@users.noreply.github.com>
2022-08-04 09:33:20 +0200

    arret debug

--- megaEpic/src/main.cpp
+++ megaEpic/src/main.cpp
@@ -10,7 +10,7 @@
 
 
 //#define DEBUG   //permet d'afficher les traces
-#define DEBUG_EXECUTION_TIME //permet d'afficher les temps d'execution
+//#define DEBUG_EXECUTION_TIME //permet d'afficher les temps d'execution
 
 
 const int anaPin[nbAnaInput] = {A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15};
